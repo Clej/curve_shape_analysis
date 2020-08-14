b1d2d4d180b73f72169ee04fce74229d06d4edcd
Guillaume-Bernard <59555086+Guillaume-Bernard@users.noreply.github.com>
2020-08-14 04:25:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+
+setup(name='curve_shape_analysis',
+      version=version,
+      description=DOCLINES[1],
+      long_description="\n".join(DOCLINES[3:]),
+      url='https://github.com/Guillaume-Bernard/curve_shape_analysis',
+      maintainer='Guillaume BERNARD',
+      maintainer_email='bernardguiugi@hotmail.fr',
+      include_package_data=True,
+      packages=find_packages(),
+      python_requires='>=3.6, <4',
+      install_requires=[
+          'cython',
+          'dcor',
+          'findiff',
+          'matplotlib',
+          'mpldatacursor',
+          'multimethod>=1.2',
+          'numpy>=1.16',
+          'pandas',
+          'rdata',
+          'scikit-datasets[cran]>=0.1.24',
+          'scikit-learn>=0.20',
+          'scipy>=1.3.0',
+          'scikit-fda'
+      ],
